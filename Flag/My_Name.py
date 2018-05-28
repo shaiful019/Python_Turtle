3c7e31f6b49b3543155aaf296190b393e600b26a
shaiful019 <shaiful019@gmail.com>
2018-05-28 20:44:24 +0600

    added my name

---
+++ Flag/My_Name.py
@@ -0,0 +1,127 @@
+import turtle
+import time 
+
+shaiful = turtle.Pen()
+shaiful.shape("turtle")
+shaiful.speed(2)
+#------------------- S --------------------
+shaiful.pencolor("blue")
+shaiful.backward(30)
+shaiful.right(90)
+shaiful.forward(15)
+shaiful.left(90)
+shaiful.forward(30)
+shaiful.right(90)
+shaiful.forward(15)
+shaiful.right(90)
+shaiful.forward(30)
+shaiful.right(180)
+shaiful.up()
+shaiful.forward(35)
+shaiful.down()
+
+#------------------- H --------------------
+shaiful.pencolor("red")
+shaiful.left(90)
+shaiful.forward(30)
+shaiful.left(180)
+shaiful.forward(15)
+shaiful.left(90)
+shaiful.forward(20)
+shaiful.left(90)
+shaiful.forward(15)
+shaiful.left(180)
+shaiful.forward(30)
+shaiful.left(90)
+shaiful.up()
+shaiful.forward(5)
+shaiful.down()
+
+#------------------- A --------------------
+shaiful.pencolor("blue")
+shaiful.left(70)
+shaiful.forward(33)
+shaiful.right(130)
+shaiful.forward(33)
+shaiful.right(180)
+shaiful.forward(16)
+shaiful.left(60)
+shaiful.forward(14)
+shaiful.left(180)
+shaiful.up()
+shaiful.forward(27)
+shaiful.right(90)
+shaiful.forward(14)
+shaiful.left(90)
+shaiful.down()
+
+#------------------- I --------------------
+
+shaiful.pencolor("red")
+shaiful.forward(10)
+shaiful.right(180)
+shaiful.forward(5)
+shaiful.right(90)
+shaiful.forward(30)
+shaiful.left(90)
+shaiful.forward(5)
+shaiful.left(180)
+shaiful.forward(10)
+shaiful.up()
+shaiful.forward(5)
+shaiful.down()
+
+#------------------- F --------------------
+
+shaiful.pencolor("blue")
+shaiful.forward(30)
+shaiful.right(180)
+shaiful.forward(30)
+shaiful.left(90)
+shaiful.forward(30)
+shaiful.right(180)
+shaiful.forward(17)
+shaiful.right(90)
+shaiful.forward(25)
+shaiful.up()
+shaiful.forward(10)
+shaiful.down()
+
+#------------------- U --------------------
+
+shaiful.pencolor("red")
+shaiful.left(90)
+shaiful.forward(13)
+shaiful.right(180)
+shaiful.forward(30)
+shaiful.left(90)
+shaiful.forward(27)
+shaiful.left(90)
+shaiful.forward(30)
+shaiful.up()
+shaiful.right(90)
+shaiful.forward(5)
+shaiful.right(90)
+shaiful.down()
+
+#------------------- L --------------------
+
+shaiful.pencolor("blue")
+shaiful.forward(30)
+shaiful.left(90)
+shaiful.forward(25)
+shaiful.right(180)
+shaiful.up()
+shaiful.forward(100)
+shaiful.left(90)
+shaiful.forward(112)
+shaiful.left(90)
+shaiful.down()
+
+
+#------------------- Circle --------------------
+
+shaiful.pencolor("red")
+shaiful.circle(130)
+
+time.sleep(3)
